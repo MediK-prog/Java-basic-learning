7cf4326a7ad574ff68a40c7c974acfdf08e8f1fd
Mahdi Kazemi <mahdi.kazemi200562@outlook.com>
2025-06-01 17:06:29 +0330

    Integrate ElementaryArithmetic into Main class

--- Cal1.2/src/Main.java
+++ Cal1.2/src/Main.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        ElementaryArithmetic calcu = new ElementaryArithmetic();
+
         String response;
 
         while (true) {
             System.out.println("Enter two numbers: ");
-            double num1 = scn.nextDouble();
-            double num2 = scn.nextDouble();
+            int num1 = scn.nextInt();
+            int num2 = scn.nextInt();
 
-            System.out.println("Addition: " + (num1 + num2));
-            System.out.println("Subtraction: " + (num1 - num2));
-            System.out.println("Multiplication: " + (num1 * num2));
+            System.out.println("Addition: " + calcu.add(num1, num2));
+            System.out.println("Subtraction: " + calcu.subtract(num1, num2));
+            System.out.println("Multiplication: " + calcu.multiply(num1, num2));
 
             if (num2 != 0) {
-                System.out.println("Division: " + (num1 / num2));
+                double division = calcu.divide(num1, num2);
+                System.out.printf("Division: %.3f\n", division);
             } else {
                 System.out.println("Cannot divide by zero!");
             }
-
 
             while (true) {
                 System.out.println("New calculation? (Type 'Yes' to continue or 'No' to exit)");
